fdaf864a06cd51ae9aadb43e3e38906e1f856939
secmeant <patryk.wlazlyn@gmail.com>
2018-07-25 17:05:03 +0200

    Adding installator in python to work both on linux and windows

---
+++ installator/instalib.py
@@ -0,0 +1,12 @@
+def fetchArg(msg, errmsg, verify):
+    isOk = False
+
+    arg = input(msg)
+    isOk = verify(arg)
+
+    while not isOk:
+        print(errmsg)
+        arg = input(msg)
+        isOk = verify(arg)
+    
+    return arg
---
+++ installator/install.py
@@ -0,0 +1,65 @@
+import os
+import sys
+from instalib import fetchArg
+from shutil import copyfile
+
+print("USP INSTALLER")
+
+# INSTALLATION FLAGS #
+INSTALL_PACKAGES = False
+
+errmsg = 'Given path is invalid.'
+INSTALLATION_PATH = os.path.abspath(fetchArg('Supply installation dir path: ', errmsg, os.path.isdir))
+SHARED_PATH = os.path.abspath(fetchArg('Supply dir from which share files: ', errmsg, os.path.isdir))
+DOWNLOAD_DIR_PATH = os.path.abspath(fetchArg('Supply dir which will store downloaded files: ', errmsg, os.path.isdir))
+SHARED_NICKNAME = fetchArg('Supply your nickname shared to others in network: ', '', lambda _unused: True)
+
+del errmsg
+
+INSTALLATION_PATH = os.path.join(INSTALLATION_PATH,'USP')
+
+CLIENT_PATH = os.path.join(INSTALLATION_PATH, 'client')
+
+SERVER_PATH = os.path.join(INSTALLATION_PATH, 'server')
+
+# Client files
+os.makedirs(CLIENT_PATH, exist_ok=True)
+
+copyfile('./client/client.py', os.path.join(CLIENT_PATH,'client.py'))
+copyfile('./client/preferences.conf', os.path.join(CLIENT_PATH,'preferences.conf'))
+
+# Server files
+os.makedirs(SERVER_PATH, exist_ok=True)
+
+copyfile('./server/main.py', os.path.join(SERVER_PATH,'main.py'))
+copyfile('./server/FileServer.py', os.path.join(SERVER_PATH,'FileServer.py'))
+copyfile('./server/sockethelpers.py', os.path.join(SERVER_PATH,'sockethelpers.py'))
+
+# Creating default ini files
+CLIENT_SETTINGS = os.path.join(CLIENT_PATH, 'settings')
+os.makedirs(CLIENT_SETTINGS, exist_ok=True)
+
+SERVER_SETTINGS = os.path.join(SERVER_PATH, 'settings')
+os.makedirs(SERVER_SETTINGS, exist_ok=True)
+
+SERVER_LOGS = os.path.join(SERVER_PATH, 'logs')
+os.makedirs(SERVER_LOGS, exist_ok=True)
+
+CLIENT_SETTINGS = os.path.join(CLIENT_SETTINGS, 'settings.ini')
+SERVER_SETTINGS = os.path.join(SERVER_SETTINGS, 'settings.ini')
+
+with open(CLIENT_SETTINGS, 'w') as f:
+    f.write('[DOWNLOAD]\n')
+    f.write('folder_path={} \n'.format(DOWNLOAD_DIR_PATH))
+    # Mocking dir name for now
+    f.write('folder_name={} \n'.format('download'))
+
+with open(SERVER_SETTINGS, 'w') as f:
+    f.write('[main]\n')
+    f.write('shared_path={} \n'.format(SHARED_PATH))
+    f.write('shared_name={} \n'.format(SHARED_NICKNAME))
+
+del CLIENT_SETTINGS
+del SERVER_SETTINGS
+del SERVER_LOGS
+
